ff707e193734a2bb098ebe3cda54ccac575b74e9
Tanner Nelson <me@tanner.xyz>
2016-07-20 12:56:20 -0400

    strand on linux

--- Package.swift
+++ Package.swift
@@ -1,9 +1,19 @@
 import PackageDescription
 
+var dependencies: [Package.Dependency] = []
+
+#if os(Linux)
+    dependencies += [
+        //Wrapper around pthreads
+        .Package(url: "https://github.com/ketzusaka/Strand.git", majorVersion: 1, minor: 5),
+    ]
+#endif
+
 let package = Package(
+    name: "Core",
     targets: [
         Target(name: "Core", dependencies: ["libc"]),
 		Target(name: "libc")
 	],
-    name: "Core"
+    dependencies: dependencies
 )
