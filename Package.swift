1bc98f3214ee6c0851c14770eae88517b7a3733d
Logan Wright <logan.william.wright@gmail.com>
2016-08-13 21:54:36 -0400

    rm dep temporarily while testing

--- Package.swift
+++ Package.swift
@@ -1,19 +1,9 @@
 import PackageDescription
 
-var dependencies: [Package.Dependency] = []
-
-#if os(Linux)
-    dependencies += [
-        //Wrapper around pthreads
-        .Package(url: "https://github.com/ketzusaka/Strand.git", majorVersion: 1, minor: 6)
-    ]
-#endif
-
 let package = Package(
     name: "Core",
     targets: [
         Target(name: "Core", dependencies: ["libc"]),
-		Target(name: "libc")
-	],
-    dependencies: dependencies
+		    Target(name: "libc")
+	  ]
 )
