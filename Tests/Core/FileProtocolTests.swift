55b50713f83b1ce88b4cf00c5ec0d8251c46e571
Tanner Nelson <me@tanner.xyz>
2016-08-19 18:43:49 -0400

    100!

--- Tests/Core/FileProtocolTests.swift
+++ Tests/Core/FileProtocolTests.swift
@@ -15,6 +15,19 @@ class FileProtocolTests: XCTestCase {
         XCTAssert(bytes.string.contains("foobar")) // inception
     }
 
+    func testLoadFail() throws {
+        let path = "!!!ferret!!!"
+        do {
+            let file = DataFile()
+            _ = try file.load(path: path)
+            XCTFail("Shouldn't have loaded")
+        } catch DataFile.Error.fileLoad(let p) {
+            XCTAssertEqual(path, p)
+        } catch {
+            XCTFail("Wrong error: \(error)")
+        }
+    }
+
     func testSave() {
         let file = DataFile()
         do {
