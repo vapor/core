6b1ba8c2d8c4f695e96bb275c005ff8b2b3bff4c
Logan Wright <logan.william.wright@gmail.com>
2016-09-07 22:23:28 -0400

    try clocks on Linux

--- Sources/Core/DispatchTime+Utilities.swift
+++ Sources/Core/DispatchTime+Utilities.swift
@@ -2,23 +2,28 @@ import libc
 import Foundation
 import Dispatch
 
+#if os(Linux)
+    let CLOCKS = CLOCKS_PER_SEC
+#else
+    let CLOCKS = NSEC_PER_SEC
+#endif
 extension Double {
     internal var nanoseconds: UInt64 {
-        return UInt64(self * Double(NSEC_PER_SEC))
+        return UInt64(self * Double(CLOCKS))
     }
 }
 
 extension UInt64 {
     internal var ts: timespec {
-        let secs = Int(self / UInt64(NSEC_PER_SEC))
-        let nsecs = Int(self % UInt64(NSEC_PER_SEC))
+        let secs = Int(self / UInt64(CLOCKS))
+        let nsecs = Int(self % UInt64(CLOCKS))
         return timespec(tv_sec: secs, tv_nsec: nsecs)
     }
 }
 
 extension timespec {
     internal var nanoseconds: UInt64 {
-        let seconds = UInt64(tv_sec) * NSEC_PER_SEC
+        let seconds = UInt64(tv_sec) * UInt64(CLOCKS)
         let nanos = UInt64(tv_nsec)
         return seconds + nanos
     }
