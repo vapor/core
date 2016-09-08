cf4f169e3c0fe981f59b7b9d200003aada458b87
Logan Wright <logan.william.wright@gmail.com>
2016-09-07 22:30:39 -0400

    use just dispatch time

--- Sources/Core/DispatchTime+Utilities.swift
+++ Sources/Core/DispatchTime+Utilities.swift
@@ -2,30 +2,9 @@ import libc
 import Foundation
 import Dispatch
 
-#if os(Linux)
-    let CLOCKS = CLOCKS_PER_SEC
-#else
-    let CLOCKS = NSEC_PER_SEC
-#endif
 extension Double {
     internal var nanoseconds: UInt64 {
-        return UInt64(self * Double(CLOCKS))
-    }
-}
-
-extension UInt64 {
-    internal var ts: timespec {
-        let secs = Int(self / UInt64(CLOCKS))
-        let nsecs = Int(self % UInt64(CLOCKS))
-        return timespec(tv_sec: secs, tv_nsec: nsecs)
-    }
-}
-
-extension timespec {
-    internal var nanoseconds: UInt64 {
-        let seconds = UInt64(tv_sec) * UInt64(CLOCKS)
-        let nanos = UInt64(tv_nsec)
-        return seconds + nanos
+        return UInt64(self * Double(1_000_000_000))
     }
 }
 
@@ -34,7 +13,7 @@ extension DispatchTime {
         Create a dispatch time for a given seconds from now.
     */
     public init(secondsFromNow: Double) {
-        let nano = timespec(secondsFromNow: secondsFromNow)
-        self.init(uptimeNanoseconds: nano.nanoseconds)
+        let uptime = DispatchTime.now().rawValue + secondsFromNow.nanoseconds
+        self.init(uptimeNanoseconds: uptime)
     }
 }
