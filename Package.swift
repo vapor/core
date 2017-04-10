c70b742db7aefc3a7132638539a27500f298de6b
tanner0101 <me@tanner.xyz>
2017-04-10 13:20:38 +0200

    core

--- Package.swift
+++ Package.swift
@@ -1,6 +1,6 @@
 import PackageDescription
 
-let beta1 = Version(1,0,0, prereleaseIdentifiers: ["beta1"])
+let beta = Version(1,0,0, prereleaseIdentifiers: ["beta"])
 
 let package = Package(
     name: "Core",
@@ -9,7 +9,7 @@ let package = Package(
         Target(name: "libc")
     ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/bits.git", beta1),
-        .Package(url: "https://github.com/vapor/debugging.git", beta1),
+        .Package(url: "https://github.com/vapor/bits.git", beta),
+        .Package(url: "https://github.com/vapor/debugging.git", beta),
     ]
 )
