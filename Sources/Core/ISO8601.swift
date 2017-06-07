73d151e460f7024390a7d83c316b2e6e49290448
Robert Agbozo <rob.agbozo@gmail.com>
2017-06-07 19:06:50 -0400

    removing explicit calendar assignment to ensure Linux support

--- Sources/Core/ISO8601.swift
+++ Sources/Core/ISO8601.swift
@@ -6,7 +6,6 @@ public struct ISO8601 {
 	
 	public init() {
 		let formatter = DateFormatter()
-		formatter.calendar = Calendar(identifier: .iso8601)
 		formatter.locale = Locale(identifier: "en_US_POSIX")
 		formatter.timeZone = TimeZone(secondsFromGMT: 0)
 		formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXXXX"
