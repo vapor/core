cd25effc0b83746792805a63468dc992a55a8580
Joannis Orlandos <joannis@orlandos.nl>
2017-08-18 16:26:05 +0200

    Empty core

--- Package.swift
+++ Package.swift
@@ -4,11 +4,6 @@ import PackageDescription
 let package = Package(
     name: "Core",
     targets: [
-        Target(name: "Core", dependencies: ["libc"]),
         Target(name: "libc")
-    ],
-    dependencies: [
-        .Package(url: "https://github.com/vapor/bits.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/debugging.git", majorVersion: 1),
     ]
 )
--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -5,15 +5,8 @@ let package = Package(
     name: "Core",
     products: [
         .library(name: "libc", targets: ["libc"]),
-        .library(name: "Core", targets: ["Core"]),
-    ],
-    dependencies: [
-        .package(url: "https://github.com/vapor/bits.git", .upToNextMajor(from: "1.1.0")),
-        .package(url: "https://github.com/vapor/debugging.git", .upToNextMajor(from: "1.1.0")),
     ],
     targets: [
         .target(name: "libc"),
-        .target(name: "Core", dependencies: ["libc", "Bits", "Debugging"]),
-        .testTarget(name: "CoreTests", dependencies: ["Core"]),
     ]
 )
