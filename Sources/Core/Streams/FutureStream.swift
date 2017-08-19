a7d64e7ec3673134b3082e12c30a4d8d32e1ad07
Joannis Orlandos <joannis@orlandos.nl>
2017-08-19 20:55:07 +0200

    Code comment

--- Sources/Core/Streams/FutureStream.swift
+++ Sources/Core/Streams/FutureStream.swift
@@ -1,4 +1,5 @@
 extension Stream where Output : FutureType {
+    /// Maps a stream containing futures using a closure that accepts the future's result
     public func stream<T>(to closure: @escaping (Output.Expectation) -> (T?)) -> StreamTransformer<Output.Expectation, T> {
         let transformer =  StreamTransformer<Output.Expectation, T>(using: closure)
         
