56ba20bab0b0807d4221ed320e1bf6b9fbf6378b
tanner0101 <me@tanner.xyz>
2017-08-19 21:35:05 -0400

    add error stream

--- Sources/Core/Streams/Stream.swift
+++ Sources/Core/Streams/Stream.swift
@@ -1,28 +1,34 @@
 public typealias Stream = InputStream & OutputStream
 
-public protocol InputStream: class {
+public protocol InputStream: BaseStream {
     associatedtype Input
-    func input(_ input: Input) throws
+    func inputStream(_ input: Input)
 }
 
-public protocol OutputStream: class {
+public protocol OutputStream: BaseStream {
     associatedtype Output
-    typealias OutputHandler = (Output) throws -> ()
-    var output: OutputHandler? { get set }
+    typealias OutputHandler = (Output) -> ()
+    var outputStream: OutputHandler? { get set }
 }
 
+public protocol BaseStream: class {
+    typealias ErrorHandler = (Error) -> ()
+    var errorStream: ErrorHandler? { get set }
+}
 
 extension OutputStream {
     public func consume(_ handler: @escaping OutputHandler) {
-        self.output = handler
+        self.outputStream = handler
     }
 
     public func stream<S: Stream>(to stream: S) -> S where S.Input == Self.Output {
-        self.output = stream.input
+        stream.errorStream = self.errorStream
+        self.outputStream = stream.inputStream
         return stream
     }
 
     public func consume<I: InputStream>(into input: I) where I.Input == Self.Output {
-        self.output = input.input
+        input.errorStream = self.errorStream
+        self.outputStream = input.inputStream
     }
 }
