89fb6d0ee4cd20448420e84fc46f98287a58328a
tanner0101 <me@tanner.xyz>
2017-08-22 11:38:44 -0400

    extendable

--- Sources/Core/Extendable.swift
+++ Sources/Core/Extendable.swift
@@ -2,5 +2,40 @@
 /// properties added in extension by using the
 /// provided dictionary.
 public protocol Extendable: class {
-    var extend: [String: Any] { get set }
+    var extend: Extend { get set }
+}
+
+/// A wrapper around a simple [String: Any]
+/// storage dictionary.
+///
+/// This wrapper is required to conform to
+/// Codable.
+///
+/// Extensions are used for convenience and should
+/// not be encoded or decoded.
+public final class Extend: Codable {
+    /// The internal storage.
+    public var storage: [String: Any]
+
+    /// Create a new extend.
+    public init() {
+        storage = [:]
+    }
+
+    /// Simply ignore this while encoding.
+    public func encode(to encoder: Encoder) throws {
+        // skip
+    }
+
+    /// Decode as an empty object.
+    public convenience init(from decoder: Decoder) throws {
+        // skip
+        self.init()
+    }
+
+    /// Allow subscripting
+    public subscript(_ key: String) -> Any? {
+        get { return storage[key] }
+        set { storage[key] = newValue }
+    }
 }
