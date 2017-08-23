8ef8f523e97b5bc20e30fd9295d9a213d95a3f9a
Tanner Nelson <me@tanner.xyz>
2017-08-23 16:40:54 -0400

    support error stream

--- Sources/Core/Future+Stream.swift
+++ Sources/Core/Future+Stream.swift
@@ -3,6 +3,8 @@ import Dispatch
 extension Future {
     /// Streams the result of this future to the InputStream
     public func stream<S: InputStream>(to stream: S, onQueue queue: DispatchQueue? = nil) where S.Input == Expectation {
-        self.then(asynchronously: queue, stream.inputStream)
+        then(on: queue, callback: stream.inputStream).catch { error in
+            stream.errorStream?(error)
+        }
     }
 }
