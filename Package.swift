caad4095e5a86aabfc6cc774dc384f56b972c110
tanner0101 <me@tanner.xyz>
2018-02-09 18:10:18 -0500

    correct dep

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", .branch("beta")),
+        .package(url: "https://github.com/vapor/async.git", .exact("1.0.0-beta.1")),
     ],
     targets: [
         .target(name: "Bits"),
