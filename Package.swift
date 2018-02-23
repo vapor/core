abde3b75952f96d3524622b0953a26ad3c4c819a
Tanner Nelson <me@tanner.xyz>
2018-02-22 21:04:00 -0500

    metadata updates

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         // Swift Promises, Futures, and Streams.
-        .package(url: "https://github.com/vapor/async.git", "1.0.0-beta.1"..<"1.0.0-beta.2"),
+        .package(url: "https://github.com/vapor/async.git", from: "1.0.0-rc"),
     ],
     targets: [
         .target(name: "Bits"),
