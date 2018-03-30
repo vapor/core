6ec05d5e128ad222b86ffdc8c3fe8f57740c22d1
tanner0101 <me@tanner.xyz>
2018-03-30 18:41:23 -0400

    replace chain methods

--- Sources/Async/Deprecated.swift
+++ Sources/Async/Deprecated.swift
@@ -1,1 +1,14 @@
-/// Nothing here yet ...
+
+extension Future {
+    @available(*, deprecated, renamed: "cascade(promise:)")
+    public func chain(to promise: Promise<T>) {
+        self.cascade(promise: promise)
+    }
+}
+
+extension Array where Element == Future<Void> {
+    @available(*, deprecated)
+    public func transform<T>(on worker: Worker, to callback: @escaping () throws -> Future<T>) -> Future<T> {
+        return flatten(on: worker).flatMap(to: T.self, callback)
+    }
+}
