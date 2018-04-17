c61a29ef08e7c8b3e836954269678c5a8fa31f3f
tanner0101 <me@tanner.xyz>
2018-04-16 22:46:57 -0400

    move to deprecated.swift file

--- Sources/Bits/Byte+Control.swift
+++ Sources/Bits/Byte+Control.swift
@@ -115,11 +115,3 @@ extension Byte {
         .newLine
     ]
 }
-
-// MARK: Deprecated
-
-extension Byte {
-    /// ~
-    @available (*, deprecated, renamed: "tilde")
-    public static let tilda: Byte = 0x7E
-}
---
+++ Sources/Bits/Deprecated.swift
@@ -0,0 +1,5 @@
+extension Byte {
+    /// ~
+    @available (*, deprecated, renamed: "tilde")
+    public static let tilda: Byte = 0x7E
+}
