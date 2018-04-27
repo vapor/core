f73005ee8d21800c3d1a75da5d6a243b445a6419
tanner0101 <me@tanner.xyz>
2018-04-27 15:15:08 -0400

    add implicit map/flatMap return types

--- Sources/Async/Deprecated.swift
+++ Sources/Async/Deprecated.swift
@@ -1,4 +1,5 @@
 extension Future {
+    /// See `cascade(promise:)`.
     @available(*, deprecated, renamed: "cascade(promise:)")
     public func chain(to promise: Promise<T>) {
         self.cascade(promise: promise)
@@ -6,6 +7,7 @@ extension Future {
 }
 
 extension Array where Element == Future<Void> {
+    /// See `flatten(on:)`.
     @available(*, deprecated)
     public func transform<T>(on worker: Worker, to callback: @escaping () throws -> Future<T>) -> Future<T> {
         return flatten(on: worker).flatMap(to: T.self, callback)
