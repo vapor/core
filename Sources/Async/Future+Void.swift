c623a85578d50aad479f0924010512295b7514a9
Tim <0xtimc@gmail.com>
2018-05-04 16:00:43 +0100

    Add convenience method for transforming to void

--- Sources/Async/Future+Void.swift
+++ Sources/Async/Future+Void.swift
@@ -13,3 +13,12 @@ extension Future where T == Void {
         return promise.futureResult
     }
 }
+
+extension Future where T == Void {
+    /// Convenience function for returning void inside a future
+    public static var void: Void {
+        return _void
+    }
+}
+
+private let _void: Void = {}()
