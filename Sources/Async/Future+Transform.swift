265c62731852b7f2a8d4983ce44b9bde2ca5f01c
Zach Rausnitz <zjraus@gmail.com>
2018-10-12 18:11:37 -0400

    allows transforming futures while ignoring errors

--- Sources/Async/Future+Transform.swift
+++ Sources/Async/Future+Transform.swift
@@ -10,4 +10,15 @@ extension Future {
             instance
         }
     }
+    
+    /// Maps the current future to contain the new type, ignoring errors.
+    ///
+    ///     user.save(on: req).transformAlways(to: HTTPStatus.ok)
+    public func transformAlways<T>(to instance: T) -> Future<T> {
+        return self.map(to: T.self) { _ in
+            instance
+        }.catchMap { _ in
+            instance
+        }
+    }
 }
