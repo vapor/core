c64f63cb21631010952f7abfef719d376ab6a441
Petro Rovenskyy <petro.rovenskyy@gmail.com>
2019-06-08 20:58:23 +0300

    iOS integration through SPM and Xcode 11 (#206)

--- Sources/Core/Thread+Async.swift
+++ Sources/Core/Thread+Async.swift
@@ -18,10 +18,10 @@ extension Thread {
     /// - parameters:
     ///     - work: Closure to be called on new thread.
     public static func async(_ work: @escaping () -> Void) {
-        if #available(OSX 10.12, *) {
+        if #available(macOS 10.12, iOS 10.0, *) {
             Thread.detachNewThread(work)
         } else {
-            fatalError("macOS 10.12 or later required to call Thread.async(_:)")
+            fatalError("macOS 10.12/iOS 10.0 or later required to call Thread.async(_:)")
         }
     }
 }
