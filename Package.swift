6d9e6f9d5817544d8115224ff723cce164c62593
Daniel Mandea <daniel.mandea@yahoo.com>
2020-07-11 14:43:13 +0300

    Bumped up package dependency to 2.0.0 fixes dependencies conflicts

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     dependencies: [
         /// Event-driven network application framework for high performance protocol servers & clients, non-blocking.
-        .package(url: "https://github.com/apple/swift-nio.git", from: "1.14.1"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
     ],
     targets: [
         .target(name: "Async", dependencies: ["NIO"]),
